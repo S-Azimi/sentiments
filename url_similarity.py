74f3cf9faa910ab9498f01d39c565feed3230736
S-Azimi <s.azimi@gmail.com>
2024-12-07 10:18:06 +0330

    add jaro-winkler

--- url_similarity.py
+++ url_similarity.py
@@ -8,6 +8,17 @@
 # Hamming Distance: Like Levenshtein but only works for strings of equal length2. 
 
 # Jaro-Winkler Distance: Designed for short strings like names and considers character transpositions. It gives more favorable ratings to strings that match from the beginning2.
+        # The Jaro similarity (simj) between two strings is defined as:
+
+        # simj = 1/3 * ( m /|s1| + m/|s2| + (m-t)/m )
+
+        # where:
+
+        # m: Number of matching characters
+        # Two characters from s1 and s2 are considered matching if they are the same and not farther than [max(|s1|, |s2|) / 2] – 1 characters apart.
+        # |s1|, |s2|: The length of the first and second strings, respectively
+        # t: Number of transpositions
+        # Calculated as the number of matching (but different sequence order) characters divided by 2.
 
 # Longest Common Subsequence (LCS): Finds the longest subsequence common to two sequences. It's useful for comparing strings where the order of characters matters.
 
