18c75b9468d6bff3e748264b4a369cf5c58c3959
S-Azimi <s.azimi@gmail.com>
2025-01-07 08:57:46 +0330

    change conf file to serve of local ip

--- ollama_connect.py
+++ ollama_connect.py
@@ -3,35 +3,30 @@ import requests
 import json
 # request to Ollama server
 
-url= "http://localhost:11434/api/generate"
+# url= "http://localhost:11434/api/generate"
+url= "http://192.168.0.192:11434/api/generate"
 headers = {'Content-Type': 'application/json'}
 data = {
     "model": "llama3.2:latest",
-    "prompt": "What is water made of?"
+    "prompt": "What is mellat bank?"
 }
+
+
 response = requests.post(url, headers=headers, data=json.dumps(data))
-
-response_text= ""
-for s in response.text.split('\n'):
-    try:
-        obj = json.loads(s)
-        response_text= response_text + obj["response"]
-
-    except json.JSONDecodeError as e:
-        print("error!")
-
-print(response_text)
-
-
-
-
 
 if response.status_code == 200:
     try:
-        print("\nJSON response:")
-        print(response.json())
+        response_text= ""
+        for s in response.text.split('\n'):
+            try:
+                obj = json.loads(s)
+                response_text= response_text + obj["response"]
+            except json.JSONDecodeError as e:
+                print(e)
+        print("\n",response_text)
+
     except json.JSONDecodeError as e:
         print("Failed to decode JSON:", e)
-else:
-    print("Error:", response.status_code, response.text)
+else: print(response.status_code)
+
 
